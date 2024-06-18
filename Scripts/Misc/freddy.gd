c7395951496f649adae1fc726f132e041d708b9a
Andrew <alexfeed1990real@gmail.com>
2024-06-18 19:48:34 +0300

    fix bugs

---
+++ Scripts/Misc/freddy.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _ready():
+	await get_tree().create_timer(1.0).timeout
+	$Video.play()
+	await $Video.finished
+	GameManager.goto_scene("res://main_menu.tscn")
--- Scripts/cutscene_1.gd
+++ Scripts/cutscene_1.gd
@@ -1,17 +1,13 @@
 extends Camera3D
 
 func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	if GameManager.is_running_on_mobile():
 		$"mobile controls".visible = true
 
 func _unhandled_input(event):
-	if event is InputEventMouseButton and not GameManager.is_running_on_mobile():
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	elif event.is_action_pressed("pause"):
+	if event.is_action_pressed("pause"):
 		$"Pause Screen".pause()
 
-func switch():
-	GameManager.goto_scene("res://Scenes/Gas Station.tscn")
-
 func _on_skip_pressed():
-	switch()
+	GameManager.goto_scene("res://Scenes/Gas Station.tscn")
