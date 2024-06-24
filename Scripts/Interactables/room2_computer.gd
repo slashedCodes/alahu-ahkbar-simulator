91b01169d752a5da0fd663a5ecc8589e2d421c00
Andrew <alexfeed1990real@gmail.com>
2024-06-24 22:36:44 +0300

    dingus

--- Scripts/Interactables/room2_computer.gd
+++ Scripts/Interactables/room2_computer.gd
@@ -42,9 +42,65 @@ func _on_decline_pressed():
 	
 
 func _on_answer_pressed():
-	if not GameManager.is_running_on_mobile(): 
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	$ui/calling.visible = true
+	$ui/decline.visible = false
+	$ui/answer.visible = false
+	$ui/freakbob.visible = false
+	
 	audio_pickup.playing = false
 	audio_alarm.playing = false
 	
-	 # voice line plays here
+	$"line 1".playing = true
+	await $"line 1".finished
+	$ui/choice.visible = true
+
+func _on_yes_pressed():
+	$ui/choice.visible = false
+	$"line 2".playing = true
+	await $"line 2".finished
+	
+	if not GameManager.is_running_on_mobile():
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	$ui.visible = false
+	$"hang up".playing = true
+	
+	set_meta("interactable", false)
+	set_meta("text", "")
+	
+	GameManager.movement(true)
+	GameManager.remove_objectives()
+	GameManager.objectives_visible(true)
+	GameManager.add_objective("exit apartment")
+	
+func _on_no_pressed():
+	$ui.visible = false
+	$"hang up".playing = true
+	
+	if not GameManager.is_running_on_mobile():
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		
+	GameManager.movement(true)
+	GameManager.remove_objectives()
+	GameManager.add_objective("go back to sleep")
+	
+	set_meta("interactable", false)
+	set_meta("text", "")
+	
+	var sleep = $"../sleep"
+	sleep.set_meta("interactable", true)
+	sleep.set_meta("text", "go back to sleep")
+	
+	await $"hang up".finished
+	
+	
+	
+	await get_tree().create_timer(4.5).timeout
+	
+	# kill the player
+	var player_body = player.get_parent().get_parent()
+	$boom.playing = true
+	
+	var sleep_ui = $"../sleep/fade"
+	sleep_ui.visible = false
+	player_body.die()
