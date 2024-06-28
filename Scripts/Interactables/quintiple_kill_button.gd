d487f821e71d1085bb735f31ca6f63ba807cbe95
Andrew <alexfeed1990real@gmail.com>
2024-06-28 10:17:51 +0300

    dingus

--- Scripts/Interactables/quintiple_kill_button.gd
+++ Scripts/Interactables/quintiple_kill_button.gd
@@ -1,8 +1,11 @@
 extends StaticBody3D
 
 func interact(player):
+	set_meta("interactable", false)
+	set_meta("text", "")
 	await get_tree().create_timer(0.7).timeout
 	$pipe.playing = true
 	$kill.visible = true
 	await $pipe.finished
 	$kill.visible = false
+	
